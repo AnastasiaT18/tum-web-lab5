2c9f80f0352b4cc8f85cf1b53306c50efda177d7
Anastasia Tiganescu <145902828+AnastasiaT18@users.noreply.github.com>
2026-03-16 11:48:54 +0200

    Lab5 - URL and Help Flag (#1)
    * Add -u flag with URL parsing, SSL support and HTML parsing
    
    * Add -h flag for help

--- go2web.py
+++ go2web.py
@@ -1,30 +1,77 @@
 import socket
 from bs4 import BeautifulSoup
 import re
+import sys
+import ssl
+
 
 
 host = "example.com"
 port = 80
 
+def parse_url(url):
+    scheme = url.split("://")[0]
+    if scheme == "http":
+        port = 80
+    elif scheme == "https":
+        port = 443
+    else:
+        raise ValueError("Unsupported URL scheme")
+    rest = url.split("://")[1]
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+    if "/" in rest:
+        host, path = rest.split("/", 1)
+        path = "/" + path
+    else:
+        host = rest
+        path = "/"
 
-request = f"GET / HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
-s.sendall(request.encode())
+    return port, host, path
+    
 
-response  = b""
-while True:
-    data = s.recv(4096)
-    if not data:
-        break
-    response += data
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: go2web -h for help")
+        sys.exit(1)
+   
+    if sys.argv[1] == "-h":
+        print("Usage:")
+        print("  go2web -u <URL>         # make an HTTP request and print the response")
+        print("  go2web -s <search-term> # search and print top 10 results")
+        print("  go2web -h               # show this help")
+    
+    elif sys.argv[1] == "-u":
+        if len(sys.argv) < 3:
+            print("Error:  -u requires a URL")
+            sys.exit(1)
+        url = sys.argv[2]
+        port, host, path = parse_url(url)
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+
+        if port == 443:
+            context = ssl.create_default_context()
+            s = context.wrap_socket(s, server_hostname=host)
+
+        request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
+        s.sendall(request.encode())
+
+        response  = b""
+        while True:
+            data = s.recv(4096)
+            if not data:
+                break
+            response += data
+        
+        s.close()
+        headers, body = response.decode().split("\r\n\r\n", 1)
 
 
-s.close()
-headers, body = response.decode().split("\r\n\r\n", 1)
+        body = re.sub(r'^[0-9a-fA-F]+\r?\n', '', body, flags=re.MULTILINE)
+        soup = BeautifulSoup(body, "html.parser")
+        print(soup.get_text(separator="\n", strip=True))
 
-
-body = re.sub(r'^[0-9a-fA-F]+\r?\n', '', body, flags=re.MULTILINE)
-soup = BeautifulSoup(body, "html.parser")
-print(soup.get_text(separator="\n", strip=True))
+    else:
+        print("Unknown flag. Use go2web -h for help")
+        sys.exit(1)
